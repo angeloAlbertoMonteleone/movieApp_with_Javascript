f3bd70e51480fade2ac1030af6ed2f76bb582d99
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-10 21:46:11 +0100

    new file with setTimeOut examples

---
+++ musa-exercise/setTimeOut.js
@@ -0,0 +1,17 @@
+  // const firsTimeOut = setTimeout(() => {
+            //     document.getElementById('title1').innerHTML = firsMovie.title;
+            //     document.getElementById('par1').innerText = firsMovie.overview;
+            //     const img = document.getElementById('img1');
+            //     img.src = IMAGE_BASE_URL + firsMovie.poster;
+            // }, 3000);
+
+
+            // const secondTimeOut = setTimeout(() => {
+            //     document.getElementById('title2').innerHTML = secondMovie.title;
+            //     document.getElementById('par2').innerText = secondMovie.overview;
+            //     const img2 = document.getElementById('img2');
+            //     img2.src = IMAGE_BASE_URL + secondMovie.poster;
+            //     document.querySelector('.movie1 .title1').style.display='none';
+            //     document.querySelector('.movie1 .image1').style.display='none';
+            // },6000);
+    
