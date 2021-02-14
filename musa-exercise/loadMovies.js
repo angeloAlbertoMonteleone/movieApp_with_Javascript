8b25f63271f40aed21668609e2fb5d043bb872b4
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-14 20:37:34 +0100

    first commmit

---
+++ musa-exercise/loadMovies.js
@@ -0,0 +1,8 @@
+function loadMoreMovies() {
+    return fetch(BASE_URL + MOVIE_TOPRATED + API_KEY + LANGUAGE_URL + PAGE_NUMBER_URL).then(
+        async (topRated) => {
+            return 
+
+        }
+    )
+}
