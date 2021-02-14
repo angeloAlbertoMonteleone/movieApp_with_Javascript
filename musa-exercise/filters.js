b19bcc8c770f06cfe9fc4ae22922fce0947b1d27
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-14 20:48:14 +0100

    film.name added to checkbox attributes

--- musa-exercise/filters.js
+++ musa-exercise/filters.js
@@ -2,15 +2,15 @@
 
 
 function createFiltersList(films) {
-
     // const genre = film.genres_id.map((idCat) => genresCat[idCat]);
-    
+
         let movie = " ";
         
     for (let film of films) {
         movie += `
-        <input id="checkbox" type="checkbox" name="${genresCat[film.genre_ids[0]]}" value="${genresCat[film.genre_ids[0]]}" onchange="moviesGenresFilter(this)">
-        <label for="${genresCat[film.genre_ids[0]]}">${genresCat[film.genre_ids[0]]}</label><br>`;
+        <input id="checkbox" type="checkbox" name="${film.name}" value="${film.name}" onchange="moviesGenresFilter()">
+        <label for="${film.name}">${film.name}</label><br>`;
+
         
         form.innerHTML = movie;        
     }    
