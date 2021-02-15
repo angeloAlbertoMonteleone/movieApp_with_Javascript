3948ad89bab8dea1f1be44d1ccfcf2087b515101
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-15 22:17:10 +0100

    folder refreshed

--- musa-exercise/loadMovies.js
+++
@@ -1,8 +0,0 @@
-function loadMoreMovies() {
-    return fetch(BASE_URL + MOVIE_TOPRATED + API_KEY + LANGUAGE_URL + PAGE_NUMBER_URL).then(
-        async (topRated) => {
-            return 
-
-        }
-    )
-}
