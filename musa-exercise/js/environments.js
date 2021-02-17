ab006cde3d1ed77a08f52569d862ed993c93fda7
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-17 20:55:37 +0100

    first commit

---
+++ musa-exercise/js/environments.js
@@ -0,0 +1,8 @@
+
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const MOVIE_TOPRATED = '/movie/top_rated';
+export const MOVIE_GENRES = '/genre/movie/list';
+export const API_KEY = '?api_key=ebe0a7b19063d864de232de72766c4ee';
+export const LANGUAGE_URL = '&language=en-US&page=';
+export let PAGE_NUMBER_URL = 1;  
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w200";
