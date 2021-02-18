3754af47775a11d3eda6b428755132a49ca37e16
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-18 22:36:44 +0100

    END_POINT_SEARCH added

--- musa-exercise/js/environments.js
+++ musa-exercise/js/environments.js
@@ -6,3 +6,4 @@ export const API_KEY = '?api_key=ebe0a7b19063d864de232de72766c4ee';
 export const LANGUAGE_URL = '&language=en-US&page=';
 export let PAGE_NUMBER_URL = 1;  
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w200";
+export let END_POINT_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=ebe0a7b19063d864de232de72766c4ee`;
