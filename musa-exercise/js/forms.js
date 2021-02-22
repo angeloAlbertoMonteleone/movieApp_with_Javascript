90a5f3d7194a9680e37ca815c8e0c3d563f2cda2
Angelo Alberto Monteleone <angeloalbertomonteleone@users.noreply.github.com>
2021-02-22 21:57:31 +0100

    createFiltersList function moved

--- musa-exercise/js/forms.js
+++ musa-exercise/js/forms.js
@@ -1,8 +1,26 @@
 import {setFilmFragForSearch, setFilmFragForReset} from "./renderMovies.js";
 import {filmTopRated} from "./fetch.js";
+import { form } from "./app.js";
 
 /* reset movies list */
 export function resetForm(){
     setFilmFragForReset(filmTopRated);
     console.log('reset form', filmTopRated) 
 }
+
+
+export function createFiltersList(films) {
+    // const genre = film.genres_id.map((idCat) => genresCat[idCat]);
+
+        let movie = " ";
+        
+    for (let film of films) {
+        movie += `
+        <input id="checkbox" type="checkbox" name="${film.name}" value="${film.name}" onchange="moviesGenresFilter()">
+        <label for="${film.name}">${film.name}</label><br>`;
+
+        
+        form.innerHTML = movie;        
+    }    
+}
+
